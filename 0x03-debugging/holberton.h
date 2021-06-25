3e70058f16552b7a69f21a63da4d9369c79cd8ec
sikakwesi2020 <sikakwesi2000@gmail.com>
2021-06-24 22:47:59 -0700

    Initial commit

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,15 +1,11 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+#include <stdio.h>
 
-#endif
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+int convert_day(int, int);
+void print_remaining_days(int, int, int);
+
+#endif /* HOLBERTON_H */
